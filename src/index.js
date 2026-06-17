71c7ec8df87893e33a103566a1153e6391c90592
boryanaK16 <boryana.knd16@gmail.com>
2026-06-17 20:26:10 +0300

    Add home page html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import http from 'http'
+import fs from 'fs/promises'
 
-const server = http.createServer((req, res) => {
-    res.write('Hello')
-    res.end()
+const server = http.createServer(async (req, res) => {
+    const homePage =  await fs.readFile('./src/views/home/index.html', 'utf-8')
+
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(homePage);
+
+    res.end();
 })
 
 server.listen(5000, () => console.log('Server is listening on port http://localhost:5000'));
